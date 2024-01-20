4961d3161e1703d968671bef921dd7af0bc04732
dhinesh <dhineshpazanisamy@gmail.com>
2024-01-20 22:28:04 +0530

    changes added

--- stream.py
+++ stream.py
@@ -4,6 +4,8 @@ st.set_page_config(page_title="Gemini AI")
 
 st.header("The FET Quest")
 
+st.write('Welcome to the blog')
+
 with st.sidebar:
     st.title("Menu")
     #pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
