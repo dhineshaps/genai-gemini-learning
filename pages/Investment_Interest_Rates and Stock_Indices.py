bf9a94c0863d5be595fc0a14b6ab41be3f559c71
Dhinesh Palanisamy <dhinesh.palanisamy@yahoo.com>
2024-01-26 23:42:30 +0530

    footer checking v6

--- pages/Investment_Interest_Rates and Stock_Indices.py
+++ pages/Investment_Interest_Rates and Stock_Indices.py
@@ -10,18 +10,6 @@ st.header("The FET Quest - Image Bot")
 
 
 footer="""<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
-
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-
 .footer {
 position: fixed;
 left: 0;
