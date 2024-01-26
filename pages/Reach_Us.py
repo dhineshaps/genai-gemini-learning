943257dfd8c25750aa8fa72498af3a313499eb9e
Dhinesh Palanisamy <dhinesh.palanisamy@yahoo.com>
2024-01-27 00:35:45 +0530

    house keeping clean up

--- pages/Reach_Us.py
+++ pages/Reach_Us.py
@@ -3,6 +3,7 @@ from PIL import Image
 
 im = Image.open('the-fet-quest.jpg')
 st.set_page_config(page_title="Reach Us", page_icon = im,layout="wide")
+
 st.write("Thanks for Visiting us")
 
 footer="""<style>
